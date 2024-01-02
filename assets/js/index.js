7739de2660b5460ba71f09d3767dab9cabe2fc80
Paulo Ricardo <pr838908@gmail.com>
2024-01-02 13:59:35 -0300

    Corrige porcentagem

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,6 +9,11 @@ $(() => {
     $(".page-9").hide();
     $(".page-10").hide();
     $(".input-group__error").hide();
+    let userName = "Não informado!";
+
+    $(".bad-test").hide();
+    $(".intermediary-test").hide();
+    $(".nice-test").hide();
 
     $("#btn-start").on('click', () => {
         $(".page-2").show(200);
@@ -51,6 +56,7 @@ $(() => {
         $(".page-3").show(200);
 
         $(".section-nome").text($("#input-name").val());
+        userName = $("#input-name").val();
     });
     // PAGE 3
     $("#page-3-continue").on('click', () => {
@@ -66,15 +72,35 @@ $(() => {
 
     let lastPosition = 360;
     let clicks = 0;
+    let hitPercentage = 0;
+    let maxNumberClicks = 16;
+    let pointForHit = 50 / maxNumberClicks;
 
-    const handlePosition = () => {
+    handlePointsByArrowClicked = (element, position) => {
+        let elementClass = $(element.currentTarget).attr('class').split(' ')[1];
+
+        let options = {
+            'game__arrow-right': () => position === 360,
+            'game__arrow-left': () => position === 180,
+            'game__arrow-top': () => position === -90,
+            'game__arrow-bottom': () => position === 90,
+        }
+
+        if(options[elementClass]()){
+            hitPercentage += pointForHit;
+        }
+    }
+
+    const handlePosition = (element) => {
         var angulos = [90, 180, -90, 360];
         var indiceAleatorio = Math.floor(Math.random() * angulos.length);
         var anguloAleatorio = angulos[indiceAleatorio];
 
         if (lastPosition == anguloAleatorio) {
-            return handlePosition();
+            return handlePosition(element);
         }
+
+        handlePointsByArrowClicked(element, lastPosition);
 
         lastPosition = anguloAleatorio;
         $(".game__test").css('transform', `rotate(${anguloAleatorio}deg)`);
@@ -85,7 +111,7 @@ $(() => {
 
             $(".game__test").css('font-size', px - 12);
 
-            if (clicks === 16) {
+            if (clicks === maxNumberClicks) {
                 $(".page-5").hide(200);
                 $(".page-6").show(200);
             }
@@ -118,5 +144,28 @@ $(() => {
     $(".btn-page-9").on('click', () => {
         $(".page-9").hide(200);
         $(".page-10").show(200);
+
+        $(".section__percentage").text(`${hitPercentage.toFixed(0)}%`);
+
+        if(hitPercentage < 50){
+            $(".section__percentage").addClass('text-danger');
+            $(".bad-test").show();
+        }else if(hitPercentage < 70) {
+            $(".section__percentage").addClass('text-warning');
+            $(".intermediary-test").show();
+        } else {
+            $(".section__percentage").addClass('text-success');
+            $(".nice-test").show();
+        }
+    });
+    // https://wa.me/16474986749
+    // href="" 
+    $(".page-10__button").on('click', (e) => {
+        e.preventDefault();
+
+        console.log('1');
+        let message = `Olá, meu nome é ${userName}. Fiz um teste e meu resultado foi ${hitPercentage.toFixed(0)}%. Gostaria de atendimento para resultados mais detalhados.`;
+
+        window.location.href = `https://wa.me/558181175028?text=${message}`;
     });
 });
